3f8bc770d84f580734fd35c3f931b9744c1fbf59
hakelimopu <hakelimopu@gmail.com>
2016-09-19 15:53:51 -0500

    mvc

--- SDL2FS.TestClient/Program.fs
+++ SDL2FS.TestClient/Program.fs
@@ -2,6 +2,30 @@
 
 open System
 
+type ViewFunc<'TModel> = 'TModel -> unit 
+
+type View<'TModel> =
+    | Single of ViewFunc<'TModel>
+    | Composite of View<'TModel> list
+
+type Controller<'TEvent, 'TModel, 'TIdentifier> when 'TIdentifier: comparison = 
+    'TEvent -> ('TModel * View<'TModel>) -> ('TModel * View<'TModel> * ('TIdentifier option))
+
+type State<'TEvent,'TModel,'TIdentifier> when 'TIdentifier: comparison =
+    {Model:'TModel;
+    View:View<'TModel>;
+    Controllers:Map<'TIdentifier, Controller<'TEvent, 'TModel,'TIdentifier>>}
+
+//let rec eventPump (eventSource:unit->'TEvent) (identifierFetcher:State<'TEvent,'TModel,'TIdentifier>->'TIdentifier) (state: State<'TEvent,'TModel,'TIdentifier>) =
+//    let event = eventSource()
+//    let identifier = state |> identifierFetcher
+//    let (m,v,i) =
+//        state.Controllers
+//        |> Map.f
+//         event (state.Model, state.View)
+//    if i.
+
+
 let rec mainMenu () =
     Console.WriteLine()
     Console.WriteLine("Main Menu:")
